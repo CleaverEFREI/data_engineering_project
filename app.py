6bd0caedf26c79fe05d159c46af1243bc3c92853
Cleaver <louis.gailhac@efrei.net>
2021-12-11 02:56:18 +0100

    Start of backend + ml dodel test

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+import numpy as np
+import pickle
+from flask import Flask, render_template, request
+
+
 app = Flask(__name__)
+
+
+model = pickle.load(open("model.pkl","rb"))
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+
+    return render_template("index.html",prediction_text = "The sentence is {}".format(prediction))
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
--- model.py
+++ model.py
@@ -1,1 +1,81 @@
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
 
+import pandas as pd
+import numpy as np
+import re
+import string
+from tqdm import tqdm
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+# ML Libraries
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+tqdm.pandas()
+
+# Global Parameters
+stop_words = set(stopwords.words('english'))
+
+def load_dataset(filename, cols):
+    dataset = pd.read_csv(filename, encoding='latin-1')
+    dataset.columns = cols
+    return dataset
+
+def remove_unwanted_cols(dataset, cols):
+    for col in cols:
+        del dataset[col]
+    return dataset
+
+def preprocess_tweet_text(tweet):
+    tweet.lower()
+    # Remove urls
+    tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+    # Remove user @ references and '#' from tweet
+    tweet = re.sub(r'\@\w+|\#','', tweet)
+    # Remove punctuations
+    tweet = tweet.translate(str.maketrans('', '', string.punctuation))
+    # Remove stopwords
+    tweet_tokens = word_tokenize(tweet)
+    filtered_words = [w for w in tweet_tokens if not w in stop_words]
+    
+    ps = PorterStemmer()
+    stemmed_words = [ps.stem(w) for w in filtered_words]
+    lemmatizer = WordNetLemmatizer()
+    lemma_words = [lemmatizer.lemmatize(w, pos='a') for w in stemmed_words]
+    
+    return " ".join(filtered_words)
+
+def get_feature_vector(train_fit):
+    vector = TfidfVectorizer(sublinear_tf=True)
+    vector.fit(train_fit)
+    return vector
+
+# Load dataset
+dataset = load_dataset("training.csv", ['target', 't_id', 'created_at', 'query', 'user', 'text'])
+# Remove unwanted columns from dataset
+n_dataset = remove_unwanted_cols(dataset, ['t_id', 'created_at', 'query', 'user'])
+#Preprocess data
+dataset.text = dataset['text'].progress_apply(preprocess_tweet_text)
+
+# Split dataset into Train, Test
+
+# Same tf vector will be used for Testing sentiments on unseen trending data
+tf_vector = get_feature_vector(np.array(dataset.iloc[:, 1]).ravel())
+X = tf_vector.transform(np.array(dataset.iloc[:, 1]).ravel())
+y = np.array(dataset.iloc[:, 0]).ravel()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=30)
+
+# Training Logistics Regression model
+LR_model = LogisticRegression(solver='lbfgs', max_iter=100000, verbose=1)
+LR_model.fit(X_train, y_train)
+y_predict_lr = LR_model.predict(X_test)
+print(accuracy_score(y_test, y_predict_lr))
